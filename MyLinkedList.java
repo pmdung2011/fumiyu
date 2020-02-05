8433ffa83c4b422b3d908cba0b6d4a72da426b62
pmdung2011 <54776410+pmdung2011@users.noreply.github.com>
2020-02-04 22:42:28 -0600

    Add files via upload

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -72,38 +72,42 @@ public class MyLinkedList {
     }
 
     //DELETION BY KEY
-    void deleteNode(int key)
+    /* Given a key, deletes all occurrence
+    of the given key in linked list */
+    void deleteKey(int key)
     {
         // Store head node
         Node temp = head, prev = null;
 
-        // If head node itself holds the key to be deleted
-        if ((temp != null) && (temp.data == key))
+        // If head node itself holds the key
+        // or multiple occurrences of key
+        while (temp != null && temp.data == key)
         {
             head = temp.next; // Changed head
-            return;
+            temp = head;         // Change Temp
         }
 
-        // Search for the key to be deleted, keep track of the
-        // previous node as we need to change temp.next
-        while (temp != null && temp.data != key)
+        // Delete occurrences other than head
+        while (temp != null)
         {
-            prev = temp;
-            temp = temp.next;
-        }
+            // Search for the key to be deleted,
+            // keep track of the previous node
+            // as we need to change 'prev->next'
+            while (temp != null && temp.data != key)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
 
-        // If key was not present in linked list
-        if (temp == null) {
-            System.out.println(key + " is not in the list.");
-            return;
-        }
+            // If key was not present in linked list
+            if (temp == null) return;
 
-        // Unlink the node from linked list
-        if(temp != null){
+            // Unlink the node from linked list
             prev.next = temp.next;
-            System.out.println(key + " found and deleted.");
-        }
 
+            //Update Temp for next iteration of outer loop
+            temp = prev.next; //temp=temp.next
+        }
     }
 
     //Method to printout the List
