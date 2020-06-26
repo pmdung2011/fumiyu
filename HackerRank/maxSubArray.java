7f289430eaf84ddc57d580e2866ab9f3ad6b7ce1
Danny Phung <pmdung2011@gmail.com>
2020-06-26 18:28:13 -0500

    +

--- HackerRank/maxSubArray.java
+++ HackerRank/maxSubArray.java
@@ -1,10 +1,8 @@
 package HackerRank;
 
-import java.util.Arrays;
-
 public class maxSubArray {
     public static int maximumSubArray(int[] nums) {
-        int max_temp =nums[0], max_final = nums[0];
+        int max_temp = nums[0], max_final = nums[0];
         for(int i =1; i < nums.length; i++){
             max_temp = Math.max(nums[i], max_temp + nums[i]);
             max_final = Math.max(max_temp, max_final);
