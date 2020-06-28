07a42a2ca909edd93192175943534f84d9d01709
Danny Phung <pmdung2011@gmail.com>
2020-06-27 22:57:10 -0500

    + class ReverseString

---
+++ HackerRank/ReverseString.java
@@ -0,0 +1,38 @@
+package HackerRank;
+
+import java.util.Arrays;
+
+//344. Reverse String
+/*
+* Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+* */
+public class ReverseString {
+    public static char[] reverseString(char[] s) {
+        for (int i = 0; i <  (s.length - 1)/2  ; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - i -1];
+            s[s.length-1-i] = temp;
+        }
+        return s;
+    }
+    //optimal solution
+    /*
+    * public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+
+        while(start!=end && start < end){
+            char temp = s[start];
+            s[start++] = s[end];
+            s[end--] = temp;
+        }
+    }
+    * */
+
+    public static void main(String[] args){
+        char[] s = {'a','b','c','d','e'};
+        System.out.println(Arrays.toString(reverseString(s)));
+
+    }
+}
