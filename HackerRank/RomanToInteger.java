2f094f810cb3af795921217cc291e3af5f76747c
Danny Phung <pmdung2011@gmail.com>
2020-07-06 22:08:07 -0500

    +

--- HackerRank/RomanToInteger.java
+++ HackerRank/RomanToInteger.java
@@ -1,4 +1,24 @@
 package HackerRank;
-
+/*
+* Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+*
+* Input: "III"
+* Output: 3
+* */
 public class RomanToInteger {
+    public static int romanToInt(String s){
+
+
+    }
+
+    public static void main (String[] args){
+        
+    }
 }
