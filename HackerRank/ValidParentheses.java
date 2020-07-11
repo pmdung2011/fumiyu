d6697ce2e0b029fd2e17b02376e0b3902308a6bb
Danny Phung <pmdung2011@gmail.com>
2020-07-10 22:15:17 -0500

    * ValidParentheses.java

--- HackerRank/ValidParentheses.java
+++ HackerRank/ValidParentheses.java
@@ -8,22 +8,33 @@ public class ValidParentheses {
         if (s.length() == 0) {
             return true;
         } else {
-            char c;
+//            char c;
             HashMap<Character, Character> map = new HashMap<>();
             map.put(')', '(');
             map.put(']', '[');
             map.put('}', '{');
 
             Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < s.length(); i++) {
-                c = s.charAt(i);
-                if (c == ')' || c == ']' || c == '}') {
-                    if (stack.isEmpty() ||  stack.peek() !=  map.get(c)) {
+//            for (int i = 0; i < s.length(); i++) {
+//                c = s.charAt(i);
+//                if (c == ')' || c == ']' || c == '}') {
+//                    if (stack.isEmpty() ||  stack.peek() !=  map.get(c)) {
+//                        return false;
+//                    } else {
+//                        stack.pop();
+//                    }
+//                } else {
+//                    stack.push(c);
+//                }
+//            }
+
+            for (char c : s.toCharArray() ) {
+                if(map.containsKey(c)){
+                    if(stack.isEmpty() || map.get(c) != stack.pop()){
                         return false;
-                    } else {
-                        stack.pop();
                     }
-                } else {
+                }
+                else{
                     stack.push(c);
                 }
             }
