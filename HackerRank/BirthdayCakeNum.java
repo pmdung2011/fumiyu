79c42902b30856ff7b058ea19e2db46aab9e330d
Danny Phung <pmdung2011@gmail.com>
2020-08-24 23:15:50 -0500

    *BirthdayCakeNum reduce Arrays.sort()

--- HackerRank/BirthdayCakeNum.java
+++ HackerRank/BirthdayCakeNum.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BirthdayCakeNum {
-    public static int sortArr (int[] arr){
+    public static int birthdayCakeCandles (int[] arr){
         int a = arr.length;
         int counter = 0;
         for (int i : arr){
@@ -12,22 +12,24 @@ public class BirthdayCakeNum {
                 counter++;
             }
         }
-
         if(counter == 0){
-            int temp = arr[0];
-            counter+=1;
-            for(int j = 1; j < a-1; j++){
-                if(temp == arr[j]){
+            int tallest = arr[0];
+            counter = 1;
+            for(int j = 1; j < a; j++){
+                if(tallest < arr[j]){
+                    tallest = arr[j];
+                    counter = 1;
+                }
+                else if(tallest == arr[j]){
                     counter++;
                 }
             }
         }
         return counter;
-
     }
 
     public static void main(String[] args){
-        int[] a = {9,5, 3,5,5,8,8,9};
-        System.out.println(sortArr(a));
+        int[] a = {3,2,1,3};
+        System.out.println(birthdayCakeCandles(a));
     }
 }
