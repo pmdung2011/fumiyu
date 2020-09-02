baf8875e2f8fbfc016b6f1370c762be54daee4dc
Danny Phung <pmdung2011@gmail.com>
2020-09-01 20:28:26 -0500

    * update methods for ContainDup.java

--- HackerRank/ContainDup.java
+++ HackerRank/ContainDup.java
@@ -1,20 +1,46 @@
 package HackerRank;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainDup {
-    public static boolean containsDuplicate(int[] nums) {
-        //Method 1:
-
-        for (int num : nums) {
-            for (int j = 1; j < nums.length; j++) {
-                System.out.println("num[i]: " + num);
-                System.out.println("nums[j] " + nums[j]);
-                System.out.println("=============");
-                if (num != nums[j]) {
+    //O(n^2)
+    public static boolean checkDup1(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                System.out.println("nums[i]: i=" + i +" " + nums[i]);
+                System.out.println("nums[j]: j=" + j + " " + nums[j]);
+                System.out.println("====================");
+                if (nums[i] == nums[j]){
                     return true;
                 }
             }
+        }
+
+        return false;
+    }
+
+    //O(nlogn)
+    public static boolean checkDup2(int[] nums){
+        Arrays.sort(nums); //O(nlogn)
+        //O(n)
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //O(n)
+    public static boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int i : nums){
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
         }
         return false;
     }
