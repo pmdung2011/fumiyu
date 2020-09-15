e1ba5d20f964f2cc03878f8b13bae72b25da0233
Danny Phung <pmdung2011@gmail.com>
2020-09-15 16:53:34 -0500

    +MaxProfit

---
+++ HackerRank/PlusOne.java
@@ -0,0 +1,33 @@
+package HackerRank;
+
+import java.util.Arrays;
+
+/*
+* Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+* */
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0 ; i--) {
+            if(digits[i] != 9){
+                digits[i]++;
+                break;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        if(digits[0] == 0){
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+        }
+        return digits;
+    }
+
+    public static void main(String[] args){
+        int[] num = { 1,2,9};
+        System.out.println(Arrays.toString(plusOne(num)));
+    }
+}
