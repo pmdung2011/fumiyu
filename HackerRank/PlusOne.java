214d511349f7f45905002d4ac99d2b32f4c0f1c8
Danny Phung <pmdung2011@gmail.com>
2020-09-15 17:05:30 -0500

    * clean PlusOne

--- HackerRank/PlusOne.java
+++ HackerRank/PlusOne.java
@@ -12,18 +12,13 @@ public class PlusOne {
         for (int i = digits.length - 1; i >= 0 ; i--) {
             if(digits[i] != 9){
                 digits[i]++;
-                break;
+                return digits;
             }
-            else{
                 digits[i] = 0;
-            }
         }
-        if(digits[0] == 0){
-            int[] result = new int[digits.length + 1];
-            result[0] = 1;
-            return result;
-        }
-        return digits;
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 
     public static void main(String[] args){
