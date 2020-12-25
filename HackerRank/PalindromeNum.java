3aa33269deae79efa9d60ba1c768dfb1e066de36
Danny Phung <pmdung2011@gmail.com>
2020-12-25 12:56:36 -0600

    *

--- HackerRank/PalindromeNum.java
+++ HackerRank/PalindromeNum.java
@@ -1,16 +1,23 @@
-class Solution {
-    public boolean isPalindrome(int x) {
+package HackerRank;
+
+class PalindromeNum {
+    public static boolean isPalindrome(int x) {
         int newNum = 0, orgNum = x;
-        if (x < 0){
+        if (x < 0) {
             return false;
         }
-        if(x <10){
+        if (x < 10) {
             return true;
         }
-        while (x > 0){
-            newNum = newNum*10 + x%10; //Reverse the org num
+        while (x > 0) {
+            newNum = newNum * 10 + x % 10; //Reverse the org num
             x /= 10;//Remove the last digit
         }
-        return newNum-orgNum == 0;
+        return newNum - orgNum == 0;
+    }
+
+    public static void main(String[] args) {
+        int num = 123;
+        System.out.println(isPalindrome(num));
     }
 }
